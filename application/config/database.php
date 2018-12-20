a25f4b03f1002c50c9f642c8eaeea0747cef4126
dev1 <dev@octopuscart.com>
2018-12-20 09:58:40 +0530

    changes

--- application/config/database.php
+++ application/config/database.php
@@ -68,7 +68,7 @@ $db['default'] = array(
 	'username' => 'j2k5e6r5_octopus',
 	'password' => 'India$2017',
 
-	'database' => 'j2k5e6r5_cctailor',
+	'database' => 'j2k5e6r5_haristailor',
 
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
--- application/views/Email/order_pdf_2.php
+++ application/views/Email/order_pdf_2.php
@@ -16,10 +16,7 @@
 <hr/>
 <h3 style="text-align: center">
 
-    <center>
-        <img src="https://costcointernational.com/costcokartfiles/costcooffer.jpg" style="height: 500px">
-
-    </center>
+   
 </h3>
 Hello {first_name} {last_name},<br/>
 As one of Hong Kong's leading custom tailors, we are proud of our international reputation for making the highest quality tailored clothing. Since past three decades, we are known across the globe for professionally hand-crafted suits, shirts, leather jackets, sports jackets and ladies dresses from Hong Kong.
